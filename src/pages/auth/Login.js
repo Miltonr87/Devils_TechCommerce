6727c8b124c276e4aa665d5e6a88b2fd9c9db65d
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-04-26 12:57:48 -0300

    Added Login with Google
    Tested!

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { auth } from '../../firebase';
+import { auth, googleAuthProvider } from '../../firebase';
 import { toast } from 'react-toastify';
 import { Button } from 'antd';
-import { MailOutlined } from '@ant-design/icons';
+import { MailOutlined,
+         GoogleOutlined  } from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 
 
@@ -68,12 +69,45 @@ const Login = ({ history }) => {
         )
     }
 
+    const googleLogin = async () => {
+        auth.signInWithPopup(googleAuthProvider)
+        .then(async (result) => {
+          
+            const { user } = result;
+            const idTokenResult = await user.getIdTokenResult();
+            dispatch({
+                type: 'LOGGED_IN_USER',
+                payload: {
+                  email: user.email,
+                  token: idTokenResult.token
+                }
+              })
+              history.push('/')     
+        }) 
+        .catch((error) => {
+            console.log(error)
+            toast.error(error.message)
+        })
+    }    
     return (
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4> Login </h4>
+                    { loading ? 
+                    <h4 className="text-danger">Loading...</h4>
+                    : 
+                    <h4>Login</h4>  }
                     {loginForm()}
+
+                    <Button onClick={googleLogin} 
+                        type="danger" 
+                        className="mb-3" 
+                        block
+                        shape="round"
+                        icon={< GoogleOutlined />}
+                        size="large">
+                        Login with Google
+                    </Button>
                 </div>
             </div>
         </div>
