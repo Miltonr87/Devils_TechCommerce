b9fa80fc4e8486d630ac91539ac0d4028ca2c601
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-05-10 14:28:28 -0300

    Routes logic

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/user', (req, res) => {
-    res.json({
-        data: "hit API user endpoint"
-    });
+router.get("/user", (req, res) => {
+  res.json({
+    data: "hit API user endpoint",
+  });
 });
 
 module.exports = router;
